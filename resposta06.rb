bb5c0e661b6d36c35d4007a29feb834d23aec1a8
Erico Costa <prof.ericocosta@gmail.com>
2020-10-08 00:46:26 -0300

    segundo commit atividades 05

--- resposta06.rb
+++ resposta06.rb
@@ -8,9 +8,24 @@ a função para efetuar as conversões terá um parâmetro formal para registrar
 Inclua um loop que permita que o usuário repita esse cálculo para novos valores de entrada todas as vezes que desejar.
 =end
 def retorno_hora(hora, minuto)
-
+  resposta = "A"
+  resposta = "P" if hora.to_i > 12 && hora.to_i <= 23
+  resposta
 end
 
 def converte_hora (hora, minuto)
-  if hora > 12 && hora <=23
+  hora_ampm = hora.to_i
+  hora_ampm = hora.to_i - 12 if hora.to_i > 12 && hora.to_i <= 23
+  resposta = "#{hora_ampm}:#{minuto} #{retorno_hora(hora, minuto)}"
 end
+
+begin
+  print 'Entre com a hora: '
+  hora = gets.to_i
+  if hora < 24
+    print 'Entre com os minutos: '
+    minuto = gets.to_i
+
+    puts converte_hora hora, minuto
+  end
+end while hora < 24
