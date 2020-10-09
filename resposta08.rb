43e2963be7664c15d2078292e4ebc8ee243ad9cc
Erico Costa <prof.ericocosta@gmail.com>
2020-10-08 22:50:10 -0300

    terceiro commit da atividade 05

--- resposta08.rb
+++ resposta08.rb
@@ -1,1 +1,3 @@
-
+=begin
+Faça uma função que informe a quantidade de dígitos de um determinado número inteiro informado.
+=end
--- resposta09.rb
+++ resposta09.rb
@@ -1,1 +1,3 @@
-
+=begin
+Reverso do número. Faça uma função que retorne o reverso de um número inteiro informado. Por exemplo: 127 -> 721.
+=end
